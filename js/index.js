9c302d0a4111efb0160b3cd5835b519d304f6d2f
Dmitry Serzhantov <Serzhantovdimitri@yandex.ru>
2023-01-16 16:33:02 +0300

    рабочий вариант

--- js/index.js
+++ js/index.js
@@ -1,29 +1,22 @@
 const popupElement = document.querySelector(".popup");
 const profileEditButton = document.querySelector(".profile__edit-button");
 const closeButtonProfile = document.querySelector(".popup__close");
+let profileTitle = document.querySelector(".profile__title");
+let profileSubtitle = document.querySelector(".profile__subtitle");
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__input_text_name");
+let jobInput = document.querySelector(".popup__input_text_job");
 
-const open_popup = function () {
+const openPopup = function () {
   popupElement.classList.add("popup_open");
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
-};
+}
 
 const closePopup = function () {
   popupElement.classList.remove("popup_open");
 };
 
-editButtonProfile.addEventListener("click", open_popup);
-closeButtonProfile.addEventListener("click", closePopup);
-
-// Находим форму в DOM
-let formElement = document.querySelector(".popup__form"); // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = document.querySelector(".popup__name-form"); // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector(".popup__profession"); // Воспользуйтесь инструментом .querySelector()
-let popupButton = document.querySelector(".popup__button");
-let valnameInput = nameInput.value;
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function handleFormSubmit(evt) {
   evt.preventDefault();
 
@@ -37,4 +30,3 @@ closeButtonProfile.addEventListener("click", closePopup);
 formElement.addEventListener("submit", handleFormSubmit);
 
 
-!!!!
