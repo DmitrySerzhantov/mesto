8610cb174a08b64df8c98f28ddd7e60694777c99
Dmitry Serzhantov <Serzhantovdimitri@yandex.ru>
2023-01-16 17:17:12 +0300

    добавил карточки при загрузки страницы изменил стиль картинок

--- js/index.js
+++ js/index.js
@@ -6,6 +6,49 @@ let profileSubtitle = document.querySelector(".profile__subtitle");
 let formElement = document.querySelector(".popup__form");
 let nameInput = document.querySelector(".popup__input_text_name");
 let jobInput = document.querySelector(".popup__input_text_job");
+const cards = document.querySelector('.cards');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+initialCards.forEach((name) => {
+  const itemCard =
+  `
+  <div class="cards__item">
+    <img class="cards__img" src='${name.link}' alt="фото из карачаевска">
+    <div class="cards__footer">
+      <h2 class="cards__text">${name.name}</h2>
+      <button class="cards__like" type="button" aria-label="лайк для карточки"></button>
+    </div>
+  </div>
+
+`
+cards.innerHTML += itemCard;
+})
+
+
 
 const openPopup = function () {
   popupElement.classList.add("popup_open");
