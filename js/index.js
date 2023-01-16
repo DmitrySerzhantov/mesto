465943d9254967eb0e7fad6afe9b2ea095087f6f
Dmitry Serzhantov <Serzhantovdimitri@yandex.ru>
2023-01-16 18:13:57 +0300

    добавил закрытие открытие попап для добавления карт

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const popupElement = document.querySelector(".popup");
+const popupElement = document.querySelector(".popup-edit-add");
 const profileEditButton = document.querySelector(".profile__edit-button");
 const closeButtonProfile = document.querySelector(".popup__close");
 let profileTitle = document.querySelector(".profile__title");
@@ -6,36 +6,38 @@ let profileSubtitle = document.querySelector(".profile__subtitle");
 let formElement = document.querySelector(".popup__form");
 let nameInput = document.querySelector(".popup__input_text_name");
 let jobInput = document.querySelector(".popup__input_text_job");
-const cards = document.querySelector('.cards');
+const cards = document.querySelector(".cards");
+const popupCardsAdd = document.querySelector(".popup-cards-add");
+const profileAddButton = document.querySelector(".profile__add-button");
+const closeButtonAddCards = document.querySelector(".popup__close-addcards");
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
 initialCards.forEach((name) => {
-  const itemCard =
-  `
+  const itemCard = `
   <div class="cards__item">
     <img class="cards__img" src='${name.link}' alt="фото из карачаевска">
     <div class="cards__footer">
@@ -43,20 +45,17 @@ initialCards.forEach((name) => {
       <button class="cards__like" type="button" aria-label="лайк для карточки"></button>
     </div>
   </div>
+`;
+  cards.innerHTML += itemCard;
+});
 
-`
-cards.innerHTML += itemCard;
-})
-
-
-
-const openPopup = function () {
+const openPopup =  () => {
   popupElement.classList.add("popup_open");
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
-}
+};
 
-const closePopup = function () {
+const closePopup = () => {
   popupElement.classList.remove("popup_open");
 };
 
@@ -67,9 +66,16 @@ function handleFormSubmit(evt) {
   profileSubtitle.textContent = jobInput.value;
   closePopup();
 }
+const openPopupAddCards = () => {
+  popupCardsAdd.classList.add("popup_open");
+};
+
+const closePopupAddCards = () => {
+  popupCardsAdd.classList.remove("popup_open");
+};
 
 profileEditButton.addEventListener("click", openPopup);
 closeButtonProfile.addEventListener("click", closePopup);
 formElement.addEventListener("submit", handleFormSubmit);
-
-
+profileAddButton.addEventListener("click", openPopupAddCards);
+closeButtonAddCards.addEventListener("click", closePopupAddCards);
