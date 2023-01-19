1502ea72ad2c9ccda27b37fa0c5d9fd6c27e3d12
Dmitry Serzhantov <Serzhantovdimitri@yandex.ru>
2023-01-19 23:16:41 +0300

    Добавил возможность добавления новой карточки

--- js/index.js
+++ js/index.js
@@ -6,50 +6,81 @@ let profileSubtitle = document.querySelector(".profile__subtitle");
 let formElement = document.querySelector(".popup__form");
 let nameInput = document.querySelector(".popup__input_text_name");
 let jobInput = document.querySelector(".popup__input_text_job");
-const cards = document.querySelector(".cards");
 const popupCardsAdd = document.querySelector(".popup-cards-add");
 const profileAddButton = document.querySelector(".profile__add-button");
 const closeButtonAddCards = document.querySelector(".popup__close-addcards");
-const textNameCard = document.querySelector(".popup__input_text_name-card");
-const imgLink = document.querySelector(".popup__input_img_link");
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
-initialCards.forEach((item) => {
-  const itemCard = `
-  <div class="cards__item">
-    <img class="cards__img" src='${item.link}' alt="фото из карачаевска">
-    <div class="cards__footer">
-      <h2 class="cards__text">${item.name}</h2>
-      <button class="cards__like" type="button" aria-label="лайк для карточки"></button>
-    </div>
-  </div>
-`;
-  cards.innerHTML += itemCard;
+const popupInputTextNameCard = document.querySelector('.popup__input_text_name-card');
+const popupInputImgLink = document.querySelector('.popup__input_img_link');
+const submitCard = document.querySelector('.submit-card');
+
+
+  const initialCards = [
+    {
+      name: "Архыз",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    },
+    {
+      name: "Челябинская область",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    },
+    {
+      name: "Иваново",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    },
+    {
+      name: "Камчатка",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    },
+    {
+      name: "Холмогорский район",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    },
+    {
+      name: "Байкал",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    },
+  ];
+
+  const cards = document.querySelector('.cards');
+  const template = document.querySelector('.element-card').content;
+
+
+const createsArrey = initialCards.map((item) => {
+  return {
+    name: item.name,
+    link: item.link
+  };
 });
+
+function rendor ()  {
+  createsArrey.forEach(rebdorCard);
+}
+
+function rebdorCard({name, link}) {
+  const addElement = template.querySelector('.cards__item').cloneNode(true);
+  addElement.querySelector('.cards__text').textContent = name;
+  addElement.querySelector('.cards__img').src = link;
+
+
+  cards.append(addElement);
+}
+rendor();
+function handCardSubmit(evt) {
+  evt.preventDefault();
+  const addNewElement = template.querySelector('.cards__item').cloneNode(true);
+  addNewElement .querySelector('.cards__text').textContent = popupInputTextNameCard.value;
+  addNewElement .querySelector('.cards__img').src = popupInputImgLink.value;
+  cards.prepend(addNewElement);
+  closePopupAddCards();
+
+}
+submitCard.addEventListener("submit", handCardSubmit);
+
+
+
+
+
+
 
 const openPopup =  () => {
   popupElement.classList.add("popup_open");
@@ -77,32 +108,10 @@ const closePopupAddCards = () => {
 };
 
 
-const saveArrayElement =  (evt) => {
-  evt.preventDefault();
-  const addFormCard = {
-    name: textNameCard.value,
-    link: imgLink.value
-  };
-  const itemCard = `
-  <div class="cards__item">
-    <img class="cards__img" src='${addFormCard.link}' alt="фото карточки">
-    <div class="cards__footer">
-      <h2 class="cards__text">${addFormCard.name}</h2>
-      <button class="cards__like" type="button" aria-label="лайк для карточки"></button>
-    </div>
-  </div>
-`;
-
-  cards.insertAdjacentHTML('afterbegin', itemCard );
-  initialCards.unshift(addFormCard);
-  closePopupAddCards();
-}
-
-
 profileEditButton.addEventListener("click", openPopup);
 closeButtonProfile.addEventListener("click", closePopup);
 formElement.addEventListener("submit", handleFormSubmit);
 profileAddButton.addEventListener("click", openPopupAddCards);
 closeButtonAddCards.addEventListener("click", closePopupAddCards);
-popupCardsAdd.addEventListener("submit", saveArrayElement);
+
 
