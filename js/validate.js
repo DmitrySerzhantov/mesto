c91d4bb30760fb2c7b20598e883f6f2262fb49d7
Dmitry Serzhantov <Serzhantovdimitri@yandex.ru>
2023-02-08 01:32:03 +0300

    Added validation of the profile form

---
+++ js/validate.js
@@ -0,0 +1,52 @@
+const formValidationConfig = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  errorClass: "popup__input_type_error",
+  buttonSelektor: ".popup__button",
+  buttonDisabledClass: "popup__button_style_disabled",
+};
+
+function enableValidation(config) {
+  const form = document.querySelector(config.formSelector);
+
+  form.addEventListener("input", () => {
+    toggleButton(form, config);
+  });
+
+  addInputLusteners(form, config);
+  toggleButton(form, config);
+}
+
+function handleFormInput(event, config) {
+  const input = event.target;
+  const inputId = input.id;
+  const errorElement = document.querySelector(`#${inputId}-error`);
+
+  if (input.validity.valid) {
+    input.classList.remove(config.errorClass);
+    errorElement.textContent = "";
+  } else {
+    input.classList.add(config.errorClass);
+    errorElement.textContent = input.validationMessage;
+  }
+}
+
+function toggleButton(form, config) {
+  const buttunSubmit = form.querySelector(config.buttonSelektor);
+  const isFormValid = form.checkValidity();
+
+  buttunSubmit.disabled = !isFormValid;
+  buttunSubmit.classList.toggle("popup__button_style_disabled", !isFormValid);
+}
+
+function addInputLusteners(form, config) {
+  const inputList = form.querySelectorAll(config.inputSelector);
+
+  inputList.forEach((item) => {
+    item.addEventListener("input", (event) => {
+      handleFormInput(event, config);
+    });
+  });
+}
+
+enableValidation(formValidationConfig);
